89454230bbef4bb333ea5d45ca6199381a0839f8
ShootingStar91 <ttu91@hotmail.com>
2022-11-10 11:49:25 +0200

    testingbranch

--- gameLogic/mapGenerator.js
+++ gameLogic/mapGenerator.js
@@ -3,6 +3,13 @@ let clientMap = [];
 let mapHeight = null;
 let mapWidth = null;
 
+/**
+ * Creates a map with given height, width and mines percentage.
+ * @param {int} height 
+ * @param {int} width 
+ * @param {int} minesPercentage 
+ * @returns Object with fields fullMap, clientMap
+ */
 function createMap(height, width, minesPercentage) {
   map = []
   clientMap = []
@@ -26,6 +33,11 @@ function inBounds(x, y) {
   return x >= 0 && x < mapWidth && y >= 0 && y < mapHeight;
 }
 
+/**
+ * Calculates percentage of mines in map
+ * @param {array} fullMap 
+ * @returns number, percentage of mines
+ */
 function calculateMinesPercentage(fullMap) {
   var amountOfMines = 0;
   for (let i = 0; i < fullMap[0].length; i++) {
